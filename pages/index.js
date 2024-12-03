adbdd0a7432e17d0a3a1bf91c10faad5c2605338
RafaellMedeiros <64844045+RafaellMedeiros@users.noreply.github.com>
2024-12-03 02:04:29 +0000

    Adiciona página inicial com mensagem de boas-vindas e altera script de teste para desenvolvimento

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+function Home() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        fontSize: "64px",
+      }}
+    >
+      Olá Mundo, Vitor e Lucas!
+    </div>
+  );
+}
+
+export default Home;
