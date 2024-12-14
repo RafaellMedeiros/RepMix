ff509e4f88dcde0a7c1c750a65acf66aa09001a7
RafaellMedeiros <64844045+RafaellMedeiros@users.noreply.github.com>
2024-12-14 14:31:05 +0000

    Altera handler de API para retornar lista de músicas com id e tom

--- pages/api/repertoire/index.js
+++ pages/api/repertoire/index.js
@@ -1,3 +1,13 @@
 export default function handler(req, res) {
-  res.status(200).json({ text: "Olá Mundo, Vitor e Lucas!" });
+  res.status(200).json({
+    songs: [
+      { id: 1, name: "Song 1", tone: "D" },
+      { id: 2, name: "Song 2", tone: "E" },
+      { id: 3, name: "Song 3", tone: "F" },
+      { id: 4, name: "Song 4", tone: "G" },
+      { id: 5, name: "Song 5", tone: "A" },
+      { id: 6, name: "Song 6", tone: "B" },
+      { id: 7, name: "Song 7", tone: "C" },
+    ],
+  });
 }
