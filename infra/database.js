02f3a9bc21fe11b03fde2c841a28a8f972f6ae17
Rafael Medeiros <rafaelfufcg@gmail.com>
2024-12-14 13:53:01 -0300

    Refactor database configuration to use environment variables and enable test connection in API handler

--- infra/database.js
+++ infra/database.js
@@ -1,6 +1,14 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("postgres://user:pass@example.com:5432/dbname");
+const sequelize = new Sequelize(
+  process.env.POSTGRES_DB,
+  process.env.POSTGRES_USER,
+  process.env.POSTGRES_PASSWORD,
+  {
+    host: process.env.POSTGRES_HOST,
+    dialect: "postgres",
+  }
+);
 
 const testConnection = async () => {
   try {
